cb708d0143ac849badb78e18a76d36629e64ef17
lucky <lucky.hw.kim@gmail.com>
2022-08-15 12:21:30 -0700

    card

---
+++ components/Button.jsx
@@ -0,0 +1,20 @@
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const CircleButton = () => {
+  return (
+    <View>
+      <Text>Button</Text>
+    </View>
+  )
+}
+
+export const RectButton = () => {
+  return (
+    <View>
+      <Text>Button</Text>
+    </View>
+  )
+}
+
---
+++ components/index.js
@@ -0,0 +1,8 @@
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+
+export {
+  HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton
+}
